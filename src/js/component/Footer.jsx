33a65b47a208a7ae666942be640e6e7d280ccbb8
bostonrex <reynaldogoitia@gmail.com>
2023-03-11 21:27:22 +0000

    entrega

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div>
+ <div className>
+  <nav className="navbar navbar-expand-lg navbar-light bg-dark p-3">
+    <div className="container-fluid d-flex justify-content-center ">
+      <a className="navbar-brand text-white fs-6 text" href="#">Copyright © Your Website 2023 </a>
+    </div>
+  </nav>
+</div>
+        
+      </div>
+    );
+};
+
+export default Footer ;
---
+++ src/js/component/Jumbotron.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Jumbotron = () => {
+    return (
+        <div className=" container jumbotron p-4 bg-light mt-3">
+        
+        <h1 className="display-4 mt-1">A Warm Welcome!</h1>
+        <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, assumenda odit similique possimus quam labore magni vitae recusandae molestias, animi quos atque voluptas aspernatur nobis, cupiditate cumque tempore eligendi nisi aliquid id officiis sint et nemo? Rem odit inventore magnam.       
+        </p>
+        
+        <a className="btn btn-primary btn-lg mb-2" href="#" role="button">Call to action!</a>
+      </div>
+    );
+};
+
+export default Jumbotron;
